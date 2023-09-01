45e9fa0a29bebaeef1406838d4916de576e3e306
JesperKoder <jesper.kodehode@gmail.com>
2023-09-01 13:30:22 +0200

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import React from "react";
+import "./App.css";
+import Layout from "./reactStuff/layout";
+import ProjectCard from "./reactStuff/projectCard.js";
+import projects from "./reactStuff/projects.js";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>best page</p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+        <Layout>
+          <div className="project-list">
+            {projects.map((project, index) => (
+              <ProjectCard
+                key={index}
+                title={project.title}
+                description={project.description}
+                image={project.image}
+              />
+            ))}
+          </div>
+        </Layout>
+      </div>
+    </>
   );
 }
 
---
+++ src/reactStuff/projects.js
@@ -0,0 +1,19 @@
+const projects = [
+  {
+    title: "My project",
+    description: "Another project",
+    image: "https://source.unsplash.com/random/1",
+  },
+  {
+    title: "Project 2",
+    description: "My newest project",
+    image: "https://source.unsplash.com/random/42",
+  },
+  {
+    title: "Project 3",
+    description: "My newest project v2",
+    image: "https://source.unsplash.com/random/43",
+  },
+];
+
+export default projects;
